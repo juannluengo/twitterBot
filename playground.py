2971113717f89d718edda02a3a64e7565fe78ead
Juan Luengo <juanluengogallego@gmail.com>
2022-10-04 13:10:38 +0200

    Update playground.py

--- playground.py
+++ playground.py
@@ -1,10 +1,10 @@
 import tweepy
 
 
-auth = tweepy.OAuthHandler('SjDFoqWbivn9u2RL6dnfUuqoP',
-                           '1U6o8Oy0DBtSQeC9H025LIyIbl3Z8mujn1sks8KBxTXte6i8UD')
-auth.set_access_token('1529083290103304192-oduTF9v0QORyzKBllWdLevGcyRN3UM',
-                      'GUdStjYP0YQgpbYzwkyNXLFCSwjvfH9z52lJgw5Wn3K9P')
+auth = tweepy.OAuthHandler('???',
+                           '???')
+auth.set_access_token('???',
+                      '???')
 
 api = tweepy.API(auth, wait_on_rate_limit=True)
 
